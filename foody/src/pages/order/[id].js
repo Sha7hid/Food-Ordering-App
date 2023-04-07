04126f64996ddd3c1b463bf54b3adc6d222f40dd
sha7hid <shahidshafadshahad@gmail.com>
2023-04-07 20:31:48 +0530

    7-4-23

--- foody/src/pages/order/[id].js
+++ foody/src/pages/order/[id].js
@@ -1,24 +1,75 @@
 import Layout from "@/components/Layout";
 import { client } from "lib/client";
-import styles from '../../styles/Order.module.css'
-export const getServerSideProps = async({params})=> {
-    const query = `*[_type == 'order' && _id == '${params.id}']`
-const order =  await client.fetch(query);
+import styles from "../../styles/Order.module.css";
+import Cooking from 'public/cooking.png'
+import Onway from 'public/onway.png'
+import {UilBill, UilBox} from '@iconscout/react-unicons'
+import Image from "next/image";
+export const getServerSideProps = async ({ params }) => {
+  const query = `*[_type == 'order' && _id == '${params.id}']`;
+  const order = await client.fetch(query);
 
-return {
-    props:{
-        order : order[0]
-    }
-}
-}
-export default function Orders({order}) {
-    return (
-      <Layout>
-        <div className={styles.container}>
-<span className={styles.heading}> 
-Order in Process
-</span>
+  return {
+    props: {
+      order: order[0],
+    },
+  };
+};
+export default function Orders({ order }) {
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <span className={styles.heading}>Order in Process</span>
+        <div className={styles.details}>
+          <div>
+            <span>Order ID</span>
+            <span>{order._id}</span>
+          </div>
+          <div>
+            <span>Customer Name</span>
+            <span>{order.name}</span>
+          </div>
+          <div>
+            <span>Phone</span>
+            <span>{order.phone}</span>
+          </div>
+          <div>
+            <span>Method</span>
+            <span>
+             {
+                order.method === 0 ? 'Cash on Delivery': 'Online Payment(Paid)'
+             }
+            </span>
+          </div>
+          <div>
+            <span>Total</span>
+            <span>$ {order.total}</span>
+          </div>
         </div>
-      </Layout>  
-    );
+
+        <div className={styles.statusContainer}>
+<div className={styles.status}>
+<UilBill width={50} height={50}/>
+<span>Payment</span>
+{order.method === 0 ?
+<span className={styles.pending}> On Delivery</span> :
+<span className={styles.completed}> Completed</span>
+}
+</div>
+<div className={styles.status}>
+<Image src={Cooking} alt="" width={50} height={50}/>
+<span>Cooking</span>
+</div>
+<div className={styles.status}>
+<Image src={Onway} alt="" width={50} height={50}/>
+<span>On Way</span>
+</div>
+<div className={styles.status}>
+<UilBox width={50} height={50}/>
+<span>Delivered</span>
+</div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
